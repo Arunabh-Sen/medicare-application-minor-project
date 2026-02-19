9a1dfafb6294cefe125f0f258e0c179719c95941
Arunabh Sen <arunabhsen100705@gmail.com>
2026-02-19 22:59:26 +0530

    added how it works section

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 import heroImg01 from '../assets/images/hero-img01.png'
 import heroImg02 from '../assets/images/hero-img02.png'
 import heroImg03 from '../assets/images/hero-img03.png'
+import icon01 from '../assets/images/icon01.png'
+import icon02 from '../assets/images/icon02.png'
+import icon03 from '../assets/images/icon03.png'
+import { Link } from 'react-router-dom'
+import { BsArrowRight } from 'react-icons/bs'
 
 const Home = () => {
   return <>
@@ -44,12 +49,97 @@ const Home = () => {
               </div>
               <div className='mt-[50px]'>
                 <img src={heroImg02} alt="" className='w-full hero__img__gap' />
-                <img src={heroImg03} alt="" className='w-full'/>
+                <img src={heroImg03} alt="" className='w-full' />
               </div>
             </div>
           </div>
         </div>
       </section>
+      {/* hero section ends */}
+      <section className="pt-0 pb-[80px]">
+        <div className="container">
+
+          {/* Heading */}
+          <div className="flex flex-col items-center text-center">
+            <h2 className="heading max-w-[400px]">
+              Providing the best medical services
+            </h2>
+
+            <p className="text__para max-w-[500px] mt-4">
+              World-class care for everyone. Our health system offers unmatched, expert health care.
+            </p>
+          </div>
+
+          {/* Cards */}
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 lg:gap-[30px] services__grid">
+
+            {/* Find Doctor Card */}
+            <div className="service__card services__grid">
+              <div className="flex items-center justify-center">
+                <img src={icon01} alt="" />
+              </div>
+
+              <div className="mt-[20px]">
+                <h2 className="text-[22px] leading-8 text-[#181a1e] font-[700]">
+                  Find a Doctor
+                </h2>
+                <p className="text-[15px] leading-6 text-gray-600 font-[400] mt-3">
+                  Easily search and connect with experienced doctors across multiple specialties, all dedicated to providing personalized and compassionate care.
+                </p>
+              </div>
+
+              <div className="service__card__arrow">
+                <span>→</span>
+              </div>
+            </div>
+
+            {/* Find Doctor Card */}
+            <div className="service__card services__grid">
+              <div className="flex items-center justify-center">
+                <img src={icon02} alt="" />
+              </div>
+
+              <div className="mt-[20px]">
+                <h2 className="text-[22px] leading-8 text-[#181a1e] font-[700]">
+                  Find a Location
+                </h2>
+                <p className="text-[15px] leading-6 text-gray-600 font-[400] mt-3">
+                  Discover nearby clinics and hospitals with ease. Access quality healthcare services at convenient locations close to you.
+                </p>
+              </div>
+
+              <div className="service__card__arrow">
+                <span>→</span>
+              </div>
+            </div>
+
+            {/* Find Doctor Card */}
+            <div className="service__card services__grid">
+              <div className="flex items-center justify-center">
+                <img src={icon03} alt="" />
+              </div>
+
+              <div className="mt-[20px]">
+                <h2 className="text-[22px] leading-8 text-[#181a1e] font-[700]">
+                  Book Appointment
+                </h2>
+                <p className="text-[15px] leading-6 text-gray-600 font-[400] mt-3">
+                  Schedule appointments in just a few clicks. Choose your preferred doctor, date, and time for a seamless booking experience.
+                </p>
+              </div>
+
+              <div className="service__card__arrow">
+                <span>→</span>
+              </div>
+            </div>
+
+          </div>
+
+        </div>
+      </section>
+
+
+
     </>
   </>
 }

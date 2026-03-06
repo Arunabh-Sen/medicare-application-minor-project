d8e2ede3b7063d666c6746bfdf852e1894171552
Arunabh Sen <arunabhsen100705@gmail.com>
2026-03-06 10:12:54 +0530

    fixed a few backend things

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,10 +6,14 @@ import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
 
 
+import { AuthContextProvider } from './context/AuthContext.jsx'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <AuthContextProvider>
+        <App />
+      </AuthContextProvider>
     </BrowserRouter>
   </React.StrictMode>,
 )
